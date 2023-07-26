10bfe2fa10a75d0dcc2fca4655b9d6e0176be44d
Khanhdao <daobakhanhbk@gmail.com>
2023-07-27 00:06:29 +0700

    add image noti

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -17,7 +17,10 @@ import 'package:flutter/foundation.dart'
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
     if (kIsWeb) {
-      return web;
+      throw UnsupportedError(
+        'DefaultFirebaseOptions have not been configured for web - '
+        'you can reconfigure this by running the FlutterFire CLI again.',
+      );
     }
     switch (defaultTargetPlatform) {
       case TargetPlatform.android:
@@ -25,7 +28,10 @@ class DefaultFirebaseOptions {
       case TargetPlatform.iOS:
         return ios;
       case TargetPlatform.macOS:
-        return macos;
+        throw UnsupportedError(
+          'DefaultFirebaseOptions have not been configured for macos - '
+          'you can reconfigure this by running the FlutterFire CLI again.',
+        );
       case TargetPlatform.windows:
         throw UnsupportedError(
           'DefaultFirebaseOptions have not been configured for windows - '
@@ -43,16 +49,6 @@ class DefaultFirebaseOptions {
     }
   }
 
-  static const FirebaseOptions web = FirebaseOptions(
-    apiKey: 'AIzaSyD80KEGgtA3H3__wvRNoq0nE5VZJ7GeThE',
-    appId: '1:1088481550003:web:e31de618ea905766421ac6',
-    messagingSenderId: '1088481550003',
-    projectId: 'motorbike-crash-theft-detect',
-    authDomain: 'motorbike-crash-theft-detect.firebaseapp.com',
-    storageBucket: 'motorbike-crash-theft-detect.appspot.com',
-    measurementId: 'G-81PJDTWJQJ',
-  );
-
   static const FirebaseOptions android = FirebaseOptions(
     apiKey: 'AIzaSyAyFilb6lt8sOtarPkP7FtvUBvHluAyOtg',
     appId: '1:1088481550003:android:397a657d6224d1f6421ac6',
@@ -68,14 +64,5 @@ class DefaultFirebaseOptions {
     projectId: 'motorbike-crash-theft-detect',
     storageBucket: 'motorbike-crash-theft-detect.appspot.com',
     iosBundleId: 'com.example.motorbikeCrashTheftDetectionSystem',
-  );
-
-  static const FirebaseOptions macos = FirebaseOptions(
-    apiKey: 'AIzaSyDy1plCqanOi-lg6rkirf6f_GP4XtzbWw0',
-    appId: '1:1088481550003:ios:6d3eec74921c5d96421ac6',
-    messagingSenderId: '1088481550003',
-    projectId: 'motorbike-crash-theft-detect',
-    storageBucket: 'motorbike-crash-theft-detect.appspot.com',
-    iosBundleId: 'com.example.motorbikeCrashTheftDetectionSystem.RunnerTests',
   );
 }
